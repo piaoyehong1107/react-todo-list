0bcca5b62ea3938b45dd479484e061ae89918120
Yehong Piao <yehongpiao@Yehongs-MacBook-Air.local>
2023-08-01 00:38:31 -0500

    sthird

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ export default function App() {
   const [newItem, setNewItem] = useState("")
   const [todos, setTodos] = useState([])
 
+  // console.log(todos)
+
   function handleSubmit(e){
     e.preventDefault()
 
@@ -18,13 +20,31 @@ export default function App() {
         }
       ]
     })
+    setNewItem("")
   }
 
+  function toggleTodo(id, complete) { 
+    // console.log(id, complete)
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return{...todo, completed: complete}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
   return (
     <>
     <form onSubmit={handleSubmit}  className="new-item-form">
     <div className="form-row">
-      <lable htmlFor="item">New Item</lable>
+      <label htmlFor="item">New Item</label>
           <input
             type="text"
             value={newItem}
@@ -36,14 +56,22 @@ export default function App() {
     </form>
     <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
           return (
-            <li>
+            <li key={todo.id}>
             <label>
-              <input type="checkbox" checked={todo.completed} />
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={e => toggleTodo(todo.id, e.target.checked)}
+                />
               {todo.title}
             </label>
-            <button className="btn btn-danger">Delete</button>
+              <button
+                className="btn btn-danger"
+                onClick={()=>deleteTodo(todo.id)}
+              >Delete</button>
             </li>
           )
         })}
